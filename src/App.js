c07c403643afc1aa11e4c28b3850864b67a4910f
tareqmonwer <tareqmonwer137@gmail.com>
2021-01-09 03:22:41 +0600

    searchbox component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import CardList from './components/card-list/card-list.component';
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/searchbox/searchbox.component";
 import "./App.css";
 
 class App extends Component {
@@ -7,31 +8,27 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
-      searchField: '',
+      searchField: "",
     };
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users => this.setState({'monsters': users}));
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
-
     const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => (
+    const filteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-    ));
+    );
 
     return (
       <div className="App">
-        <input
-          type="search"
+        <SearchBox
           placeholder="search monsters"
-          onChange={(e) => {
-            this.setState({ searchField: e.target.value });
-          }}
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
